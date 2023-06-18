031da5a1488c81b16e7425554dddb1c6658cfe80
Emmett Childs <emmettchilds@thegobospot.com>
2023-06-18 20:43:15 +0800

    chore: modify routes

--- pkg/routes/readinglist-routes.go
+++ pkg/routes/readinglist-routes.go
@@ -1,15 +1,15 @@
 package routes
 
 import (
-	"reading-list/pkg/controllers"
+	"readinglist-api/pkg/controllers"
 
 	"github.com/gorilla/mux"
 )
 
-var UseReadinglistRoutes = func(router *mux.Router) {
-	router.HandleFunc("/", controllers.GetList).Methods("GET")
-	router.HandleFunc("/readinglist/{id}", controllers.GetBook).Methods("GET")
+var UseReadingListRoutes = func(router *mux.Router) {
+	router.HandleFunc("/readinglist", controllers.GetList).Methods("GET")
 	router.HandleFunc("/readinglist", controllers.AddBook).Methods("POST")
+	router.HandleFunc("/readinglist/{id}", controllers.GetBook).Methods("GET")
 	router.HandleFunc("/readinglist/{id}", controllers.Modify).Methods("PUT")
 	router.HandleFunc("/readinglist/{id}", controllers.Delete).Methods("DELETE")
 }
